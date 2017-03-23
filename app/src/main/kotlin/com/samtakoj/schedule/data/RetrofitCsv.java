3b5db82a2c8fb12f6364ec2fb336f487154ba889
Artsiom <artkostm@gmail.com>
2017-03-23 18:56:45 +0300

    added converter factory for Retrofit csv support

--- app/src/main/kotlin/com/samtakoj/schedule/data/RetrofitCsv.java
+++ app/src/main/kotlin/com/samtakoj/schedule/data/RetrofitCsv.java
@@ -4,16 +4,22 @@ import com.google.common.collect.Lists;
 import com.nytimes.android.external.store.base.Parser;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.ResponseBody;
 import okio.BufferedSource;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
 
 /**
  * Created by artsiom.chuiko on 21/03/2017.
@@ -34,26 +40,44 @@ public final class RetrofitCsv {
         int value() default  0;
     }
 
-    public static <Parsed> Parser<BufferedSource, List<Parsed>> createSourceParser(final Class<Parsed> clazz, final boolean skipHeaders, final String reges) {
+    public static <Parsed> Parser<BufferedSource, List<Parsed>> createSourceParser(final Class<Parsed> clazz, final boolean skipHeaders, final String regex) {
         return new Parser<BufferedSource, List<Parsed>>() {
             @Override
             public List<Parsed> call(BufferedSource source) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(source.inputStream(), Charset.forName("UTF-8")))) {
-                    String line;
-                    boolean skip = skipHeaders;
-                    final List<Parsed> parsed = Lists.newArrayList();
-                    while ((line = reader.readLine()) != null) {
-                        if (skip) skip = false;
-                        else {
-                            parsed.add(createObject(clazz, line.split(reges)));
-                        }
-                    }
-                    return parsed;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+                return convertSourceToList(source, clazz, skipHeaders, regex);
             }
         };
+    }
+
+    public static <T> Converter.Factory creteConverterFactory(final Class<T> clazz, final boolean skipHeaders, final String regex) {
+        return new Converter.Factory() {
+            @Override
+            public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
+                return new Converter<ResponseBody, List<T>>() {
+                    @Override
+                    public List<T> convert(ResponseBody value) throws IOException {
+                        return convertSourceToList(value.source(), clazz, skipHeaders, regex);
+                    }
+                };
+            }
+        };
+    }
+
+    private static <T> List<T> convertSourceToList(final BufferedSource source, final Class<T> clazz, final boolean skipHeaders, final String regex) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(source.inputStream(), Charset.forName("UTF-8")))) {
+            String line;
+            boolean skip = skipHeaders;
+            final List<T> parsed = Lists.newArrayList();
+            while ((line = reader.readLine()) != null) {
+                if (skip) skip = false;
+                else {
+                    parsed.add(createObject(clazz, line.split(regex)));
+                }
+            }
+            return parsed;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static <Parsed> Parsed createObject(Class<Parsed> clazz, String...data) throws IllegalAccessException, InvocationTargetException, InstantiationException {
