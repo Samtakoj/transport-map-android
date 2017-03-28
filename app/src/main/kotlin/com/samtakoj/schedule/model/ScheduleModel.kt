5a127e4f52ef4c029b9519770b239bbab03b9190
Artsiom <artkostm@gmail.com>
2017-03-28 14:31:56 +0300

    fix models

--- app/src/main/kotlin/com/samtakoj/schedule/model/ScheduleModel.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/model/ScheduleModel.kt
@@ -3,7 +3,7 @@ package com.samtakoj.schedule.model
 import com.samtakoj.schedule.data.RetrofitCsv.Position
 
 data class StopCsv(@param:Position(1) var id: Int, @param:Position(5) var name: String, @param:Position(7) var lng: Long, @param:Position(8) var ltd: Long)
-data class RouteCsv(@field:Position(1) var num: String, @field:Position(4) var transportType: String, @field:Position(11) var name: String,
-                    @field:Position(12) var weekDays: String, @field:Position(13) var id: Int, @field:Position(15) var stops: String)
+data class RouteCsv(@param:Position(1) var num: String, @param:Position(4) var transportType: String, @param:Position(11) var name: String,
+                    @param:Position(12) var weekDays: String, @param:Position(13) var id: Int, @param:Position(15) var stops: String)
 data class TimeCsv(val routeId: Int, val intervalCount: Int, val timeTable: List<Long>, val workDay: List<WorkDay>)
 data class WorkDay(val weekDay: String, val countInterval: Int)
