5a7aa16bf7909cef25dfb05bbd94b01745c2ba9b
Artsiom <artkostm@gmail.com>
2017-03-30 20:47:53 +0300

    added layout component

---
+++ app/src/main/kotlin/com/samtakoj/schedule/Layout.kt
@@ -0,0 +1,37 @@
+package com.samtakoj.schedule
+
+import android.support.v4.content.ContextCompat
+import android.view.View
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.toolbar
+
+/**
+ * Created by artsiom.chuiko on 30/03/2017.
+ */
+class MainActivityUi: AnkoComponent<TestActivity> {
+    companion object {
+        val StatusID = View.generateViewId()
+        val ToolbarID = View.generateViewId()
+        val LocationID = View.generateViewId()
+        val ContainerID = View.generateViewId()
+    }
+
+    override fun createView(ui: AnkoContext<TestActivity>) = with(ui) {
+        verticalLayout {
+            lparams(width = matchParent, height = matchParent)
+            gravity = 30
+            backgroundColor = ContextCompat.getColor(ctx, android.R.color.white)
+            toolbar {
+                id = ToolbarID
+                backgroundColor = ContextCompat.getColor(ctx, R.color.colorPrimary)
+            }.lparams(width = matchParent, height = wrapContent).let {
+                (ui.ctx  as TestActivity).setSupportActionBar(it)
+            }
+
+            frameLayout {
+                id = ContainerID
+                lparams(width = matchParent, height = matchParent, weight = 1f)
+            }
+        }
+    }
+}
