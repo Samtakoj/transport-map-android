de0b5b896ac26d81aa4753f9f396d451ce302b41
alex-chuyko <alex.chuyko9@gmail.com>
2017-04-03 23:24:17 +0300

    add filter in method stops in ScheduleFetcher

--- app/src/main/kotlin/com/samtakoj/schedule/MainActivity.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/MainActivity.kt
@@ -8,8 +8,11 @@ import android.location.LocationListener
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import com.google.common.collect.Lists
 import com.samtakoj.schedule.api.LocListener
+import com.samtakoj.schedule.model.RouteCsv
 import com.samtakoj.schedule.model.StopCsv
+import com.samtakoj.schedule.model.TimeCsv
 import io.nlopez.smartlocation.SmartLocation
 import org.jetbrains.anko.*
 import retrofit2.HttpException
@@ -44,8 +47,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        subscription = ScheduleFetcher.stops(application as TransportApplication, object: Subscriber<StopCsv>() {
+//        val stops = Lists.newArrayList<StopCsv>()
+        val subscriber = object: Subscriber<StopCsv>() {
             override fun onCompleted() {
+                Log.i("TRANSPORT_SCHEDULE", "Stops is completed!")
             }
 
             override fun onError(e: Throwable) {
@@ -53,9 +58,26 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNext(stop: StopCsv) {
-                Log.i("TRANSPORT_SCHEDULE", "Stop: ${stop.name}")
+//                stops.add(stop)
+                Log.i("TRANSPORT_SCHEDULE", "Stop: ${stop.id}; ${stop.name}; ${stop.lng * 0.00001}; ${stop.ltd * 0.00001}")
             }
-        })
+        }
+//        location?.latitude = 53.929018
+//        location?.longitude = 27.585731  2758469;5392808
+        subscription = ScheduleFetcher.stops(application as TransportApplication, subscriber, 53.927010, 27.576126) //53.929018, 27.585731
+//        subscription = ScheduleFetcher.routes(application as TransportApplication, object: Subscriber<RouteCsv>() {
+//            override fun onCompleted() {
+//                Log.i("TRANSPORT_SCHEDULE", "Routes is completed!")
+//            }
+//
+//            override fun onError(e: Throwable) {
+//                e.printStackTrace()
+//            }
+//
+//            override fun onNext(route: RouteCsv) {
+//                Log.i("TRANSPORT_SCHEDULE", "Route: ${route.id}; ${route.name}; ${route.num}; ${route.weekDays}; ${route.transportType}; ${route.stops}")
+//            }
+//        })
 
         SmartLocation.with(this).location().oneFix().start { location ->
             toast("Lat: ${location.latitude}, Long: ${location.longitude}")
--- app/src/main/kotlin/com/samtakoj/schedule/api/ScheduleFetcher.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/api/ScheduleFetcher.kt
@@ -1,5 +1,6 @@
 package com.samtakoj.schedule.api
 
+import android.location.Location
 import com.nytimes.android.external.store.base.impl.BarCode
 import com.samtakoj.schedule.TransportApplication
 import com.samtakoj.schedule.model.RouteCsv
@@ -9,6 +10,7 @@ import rx.Observable
 import rx.Subscriber
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
+import rx.functions.Func1
 import rx.schedulers.Schedulers
 
 /**
@@ -22,19 +24,46 @@ object ScheduleFetcher {
     //        val csvStops = app.persistedStopStore.get(BarCode("Stop", "stops"))
     //        val csvRoutes = app.persistedRouteStore.get(BarCode("Route", "routes"))
     //        val csvTimes = app.persistedTimeStore.get(BarCode("Time", "times"))
-    fun stops (app: TransportApplication, subscriber: Subscriber<StopCsv>): Subscription  {
+    fun stops (app: TransportApplication, subscriber: Subscriber<StopCsv>, latitude: Double, longitude: Double): Subscription  {
+        var previous = StopCsv(1, "", 1, 1)
         return app.persistedStopStore.get(BarCode("Stop", "stops"))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMap { Observable.from(it) }
+                .map({ stop ->
+                    stop.name = if (stop.name != "") stop.name else previous.name
+                    stop.id = if (stop.id != 0) stop.id else previous.id
+                    stop.lng = if (stop.lng != 0.toLong()) stop.lng else previous.lng
+                    stop.ltd = if (stop.ltd != 0.toLong()) stop.ltd else previous.ltd
+                    previous = stop
+                    return@map stop
+                })
+                .filter { (id, name, lng, ltd) ->
+                    val dx = Math.abs(lng * 0.00001 - longitude) * 67.138
+                    val dy = Math.abs(ltd * 0.00001 - latitude) * 111.321
+                    return@filter Math.sqrt(dx * dx + dy * dy) <= 0.25
+//                    ((Math.abs(lng * 0.00001 - longitude) <= 0.001) &&
+//                            (Math.abs(ltd * 0.00001 - latitude) <= 0.001))
+                }
                 .subscribe(subscriber)
     }
 
     fun routes (app: TransportApplication, subscriber: Subscriber<RouteCsv>): Subscription {
+        var previous = RouteCsv("", "", "", "", 1, "")
         return app.persistedRouteStore.get(BarCode("Route", "routes"))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .flatMap { Observable.from(it) }
+                .map({ route ->
+                    route.id = if (route.id != 0) route.id else previous.id
+                    route.name = if (route.name != "") route.name else previous.name
+                    route.num = if (route.num != "") route.num else previous.name
+                    route.stops = if (route.stops != "") route.stops else previous.stops
+                    route.transportType = if (route.transportType != "") route.transportType else previous.transportType
+                    route.weekDays = if (route.weekDays != "") route.weekDays else previous.weekDays
+                    previous = route
+                    return@map route
+                })
                 .subscribe(subscriber)
     }
 
