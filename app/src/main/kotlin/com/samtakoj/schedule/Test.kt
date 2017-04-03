a733abce35edbd8ec651da7b6360f3919d40589b
Artsiom <artkostm@gmail.com>
2017-04-03 20:06:49 +0300

    test example

--- app/src/main/kotlin/com/samtakoj/schedule/Test.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/Test.kt
@@ -1,15 +1,22 @@
 package com.samtakoj.schedule
 
+import android.app.Activity
 import android.app.Fragment
+import android.app.FragmentManager
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.*
 import android.widget.TextView
+import com.beyondar.android.fragment.BeyondarFragmentSupport
+import com.beyondar.android.world.GeoObject
+import com.beyondar.android.world.World
 import io.nlopez.smartlocation.SmartLocation
 import com.google.android.gms.location.DetectedActivity
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder
@@ -49,8 +56,44 @@ class TestActivity : AppCompatActivity() {
         lat.text = "%.2f".format(location?.latitude)
         lng.text = "%.2f".format(location?.longitude)
 
-        val fragment = fragmentManager.findFragmentByTag("TestFragment")?: TestFragment()
-        fragmentManager.beginTransaction().replace(MainActivityUi.ContainerID, fragment, "TestFragment").commit()
+        val world = World(this)
+        world.setDefaultImage(R.mipmap.ic_launcher)
+        world.setLocation(location)
+
+        val obj1 = GeoObject(3)
+        obj1.setLocation(location)
+        obj1.imageUri = "http://beyondar.com/sites/default/files/logo_reduced.png"
+        obj1.name = "Name"
+
+        val obj2 = GeoObject(4)
+        obj2.setGeoPosition(41.26518966360719, 1.92582424468222)
+        obj2.imageUri = "http://beyondar.com/sites/default/files/logo_reduced.png"
+        obj2.name = "Name1"
+
+        val obj3 = GeoObject(5)
+        obj3.setGeoPosition(41.26550959641445, 1.925873388087619)
+        obj3.setImageResource(R.drawable.flymer)
+        obj3.name = "Name2"
+
+        val obj4 = GeoObject(6)
+        obj4.setGeoPosition(41.26518862002349, 1.925662767707665)
+        obj4.setImageResource(R.drawable.flymer)
+        obj4.name = "Name3"
+
+        world.addBeyondarObject(obj1)
+        world.addBeyondarObject(obj2)
+        world.addBeyondarObject(obj3)
+        world.addBeyondarObject(obj4)
+
+        val fragment = supportFragmentManager.findFragmentByTag("TestFragment")?: createBeyondARFragment(this)
+        supportFragmentManager.beginTransaction().replace(MainActivityUi.ContainerID, fragment, "TestFragment").commit()
+
+        supportFragmentManager.registerFragmentLifecycleCallbacks(object: android.support.v4.app.FragmentManager.FragmentLifecycleCallbacks(){
+            override fun onFragmentViewCreated(fm: android.support.v4.app.FragmentManager?, f: android.support.v4.app.Fragment?, v: View?, savedInstanceState: Bundle?) {
+                super.onFragmentViewCreated(fm, f, v, savedInstanceState)
+                (fragment as BeyondarFragmentSupport).world = world
+            }
+        }, true)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -70,7 +113,7 @@ class TestActivity : AppCompatActivity() {
                     SmartLocation.with(this).
                             location(provider).
                             start { location ->
-                                textView1.text = "Lat: ${location?.latitude}, Lng: ${location?.longitude}"
+                                //textView1.text = "Lat: ${location?.latitude}, Lng: ${location?.longitude}"
                             }
                 } else {
                     // permission denied, boo! Disable the
@@ -85,6 +128,11 @@ class TestActivity : AppCompatActivity() {
         SmartLocation.with(this).location().stop()
         SmartLocation.with(this).activity().stop()
         super.onDestroy()
+    }
+
+    private fun createBeyondARFragment(activity: Activity): BeyondarFragmentSupport {
+        val fragment = BeyondarFragmentSupport()
+        return fragment
     }
 
     class TestFragment: Fragment() {
