5524500945eb8ed692cdeb7a14e08e9a63bc3a2e
Artsiom <artkostm@gmail.com>
2017-04-04 17:20:22 +0300

    added utils

---
+++ app/src/main/kotlin/com/samtakoj/schedule/Util.java
@@ -0,0 +1,15 @@
+package com.samtakoj.schedule;
+
+import android.view.View;
+
+/**
+ * Created by arttsiom.chuiko on 04/04/2017.
+ */
+
+public final class Util {
+    private Util() {}
+
+    public static View nullHack() {
+        return null;
+    }
+}
