7d35d5f73f078fc04396b8a715526f72ee67dd21
Artsiom <artkostm@gmail.com>
2017-04-21 22:22:38 +0300

    continued implementation

--- app/src/main/kotlin/com/samtakoj/schedule/service/ScheduleUpdateService.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/service/ScheduleUpdateService.kt
@@ -3,12 +3,34 @@ package com.samtakoj.schedule.service
 import com.google.android.gms.gcm.GcmNetworkManager
 import com.google.android.gms.gcm.GcmTaskService
 import com.google.android.gms.gcm.TaskParams
+import com.nytimes.android.external.store.base.impl.BarCode
+import com.samtakoj.schedule.TransportApplication
+import com.samtakoj.shedule.model.RouteCsv
+import com.samtakoj.shedule.model.StopCsv
+import com.samtakoj.shedule.model.TimeCsv
+import rx.Observer
+import rx.android.schedulers.AndroidSchedulers
 
 /**
  * Created by artsiom.chuiko on 19/04/2017.
  */
 class ScheduleUpdateService: GcmTaskService() {
+
     override fun onRunTask(p0: TaskParams?): Int {
+        val app = application as TransportApplication
+        val stopBox = app.boxStore.boxFor(StopCsv::class.java)
+        val routeBox = app.boxStore.boxFor(RouteCsv::class.java)
+        val timeBox = app.boxStore.boxFor(RouteCsv::class.java)
+
+        app.persistedStopStore.get(BarCode("Stop", "stops"))
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { stops ->
+                    if (stops.size > 0) {
+                        stopBox.removeAll()
+                        stopBox.put(stops)
+                    }
+                }
         return GcmNetworkManager.RESULT_SUCCESS
     }
 }
