063782c06c39982f5d7bd63fc325aed22f3a4804
Artsiom <artkostm@gmail.com>
2018-02-18 16:25:32 +0300

    update models

--- app/src/main/kotlin/com/samtakoj/schedule/data/WorkDayConverter.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/data/WorkDayConverter.kt
@@ -1,7 +1,28 @@
 package com.samtakoj.schedule.data
 
+import com.samtakoj.shedule.model.WorkDay
+import io.objectbox.converter.PropertyConverter
+import java.util.*
+
 /**
- * Created by arttsiom.chuiko on 18/02/2018.
+ * Created by artsiom.chuiko on 18/02/2018.
  */
-class WorkDayConverter {
+class WorkDayConverter : PropertyConverter<List<WorkDay>, String> {
+    override fun convertToDatabaseValue(days: List<WorkDay>?): String? {
+        if (days == null || days.isEmpty()) return null
+        return days.joinToString(ITEM_DELIMITER) { "${it.weekDay}$VAL_DELIMITER${it.countInterval}" }
+    }
+
+    override fun convertToEntityProperty(s: String?): List<WorkDay> {
+        if (s == null || s.trim().isEmpty()) return Collections.emptyList()
+        return s.split(ITEM_DELIMITER).map {
+            val vals = it.split(VAL_DELIMITER)
+            WorkDay(vals[0], vals[1].toInt())
+        }
+    }
+
+    companion object {
+        val ITEM_DELIMITER = ";"
+        val VAL_DELIMITER = "-"
+    }
 }
--- app/src/main/kotlin/com/samtakoj/schedule/model/ScheduleModel.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/model/ScheduleModel.kt
@@ -1,8 +1,10 @@
 package com.samtakoj.schedule.model
 
-import com.samtakoj.shedule.model.RouteCsv
-import com.samtakoj.shedule.model.StopCsv
-import com.samtakoj.shedule.model.TimeCsv
+import com.samtakoj.schedule.data.LongListConverter
+import com.samtakoj.schedule.data.WorkDayConverter
+import io.objectbox.annotation.Convert
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
 
 data class StopCsv1(var id: Int, var name: String, var lng: Long, var ltd: Long)
 data class RouteCsv1(var num: String, var transportType: String, var name: String,
@@ -10,3 +12,14 @@ data class RouteCsv1(var num: String, var transportType: String, var name: Strin
 data class TimeCsv1(val routeId: Int, val intervalCount: Int, val timeTable: List<Long>, val workDay: List<WorkDay1>)
 data class WorkDay1(val weekDay: String, val countInterval: Int)
 data class TestModel(val route: RouteCsv, val stops: List<StopCsv>, val times: TimeCsv)
+
+data class WorkDay(val weekDay: String, val countInterval: Int)
+@Entity
+data class TimeCsv(@Id var id: Long = 0, val routeId: Int, val intervalCount: Int,
+                   @Convert(converter = LongListConverter::class, dbType = String::class) val timeTable: List<Long> = listOf(),
+                   @Convert(converter = WorkDayConverter::class, dbType = String::class) val workDay: List<WorkDay> = listOf())
+@Entity
+data class StopCsv(@Id(assignable = true) var id: Long = 0, var name: String, var lng: Long, var ltd: Long)
+@Entity
+data class RouteCsv(@Id(assignable = true) var id: Long = 0, var num: String, var transportType: String,
+                    var name: String, var weekDays: String, var stops: String)
