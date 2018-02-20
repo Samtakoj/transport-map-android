e58bfae5fff678cb89dde0f90a107d22932bc95c
Artsiom <artkostm@gmail.com>
2018-02-20 19:29:34 +0300

    add example

--- app/src/main/kotlin/com/samtakoj/schedule/SplashActivity.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/SplashActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.samtakoj.schedule.api.ScheduleFetcher
+import android.util.Log
+import com.samtakoj.schedule.extensions.transportApp
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.setContentView
 
 /**
@@ -15,7 +18,24 @@ class SplashActivity: AppCompatActivity(), RequestPermissionCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        ScheduleFetcher.loadIfEmpty(application as TransportApplication)
+        transportApp().stopApi.fetch()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({}, {error ->
+                    Log.e(BuildConfig.APPLICATION_ID, "Error while fetching stops: ${error.message}")
+                })
+        transportApp().timeApi.fetch()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({}, {error ->
+                    Log.e(BuildConfig.APPLICATION_ID, "Error while fetching times: ${error.message}")
+                })
+        transportApp().routeApi.fetch()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({}, {error ->
+                    Log.e(BuildConfig.APPLICATION_ID, "Error while fetching routes: ${error.message}")
+                })
 
         SplashActivityUi().setContentView(this)
     }
@@ -23,7 +43,7 @@ class SplashActivity: AppCompatActivity(), RequestPermissionCallback {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
             ApplicationPermissions.INITIAL_REQUEST -> {
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     startActivity(Intent(this, MainActivity::class.java))
                 }
                 finish()
---
+++ app/src/main/kotlin/com/samtakoj/schedule/extensions/Activity.ext.kt
@@ -0,0 +1,10 @@
+package com.samtakoj.schedule
+
+import android.app.Activity
+
+/**
+ * Created by artsiom.chuiko on 20/02/2018.
+ */
+fun Activity.transportApp() : TransportApplication {
+    
+}
