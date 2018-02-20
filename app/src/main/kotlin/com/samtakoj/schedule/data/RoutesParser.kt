02a24869c291dd8b22ea45fc4471fe2af9861522
Artsiom <artkostm@gmail.com>
2018-02-20 15:57:23 +0300

    routes parser

--- app/src/main/kotlin/com/samtakoj/schedule/data/RoutesParser.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/data/RoutesParser.kt
@@ -1,7 +1,34 @@
 package com.samtakoj.schedule.data
 
+import com.samtakoj.schedule.model.RouteCsv
+import io.objectbox.BoxStore
+
 /**
- * Created by arttsiom.chuiko on 20/02/2018.
+ * Created by artsiom.chuiko on 20/02/2018.
  */
-class RoutesParser {
+class RoutesParser(private val store: BoxStore, skipHeaders: Boolean, regex: String)
+    : CsvParser<RouteCsv>(skipHeaders, regex) {
+    override fun defaultObject(): RouteCsv = RouteCsv(0, "", "", "", "", "")
+
+    override fun createObject(prev: RouteCsv, vararg data: String): RouteCsv {
+        val num = data[0]
+        val transportType = data[3]
+        val name = data[10]
+        val weekDays = data[11]
+        val id = data[12].toLong()
+        val stops = data[14]
+        return RouteCsv(
+                id = if (id != 0L) id else prev.id,
+                num = if (num.trim().isNotEmpty()) num else prev.num,
+                transportType = if (transportType.trim().isNotEmpty()) transportType else prev.transportType,
+                name = if (name.trim().isNotEmpty()) name else prev.name,
+                weekDays = if (weekDays.trim().isNotEmpty()) weekDays else prev.weekDays,
+                stops = if (stops.trim().isNotEmpty()) stops else prev.stops
+        )
+    }
+
+    override fun afterParsing(parsed: List<RouteCsv>) {
+        val routeBox = store.boxFor(RouteCsv::class.java)
+        routeBox.put(parsed)
+    }
 }
