712b4be0e3450fa3d6ee3c6013dd21d2f5556339
Artsiom <artkostm@gmail.com>
2018-02-20 16:39:45 +0300

    update api

--- app/src/main/kotlin/com/samtakoj/schedule/api/Api.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/api/Api.kt
@@ -1,7 +1,11 @@
 package com.samtakoj.schedule.api
 
+import com.samtakoj.schedule.BuildConfig
 import io.reactivex.Single
 import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -13,4 +17,16 @@ interface Api {
     @GET("/city/minsk/{dataType}.txt")
     fun fetchData(@Path("dataType") dataType: String): Single<ResponseBody>
 
+    companion object {
+
+        fun provideRetrofit(parserSupplier: () -> Converter.Factory) : Api {
+            return  Retrofit.Builder()
+                    .baseUrl("http://minsktrans.by/")
+                    .addConverterFactory(parserSupplier())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .validateEagerly(BuildConfig.DEBUG)
+                    .build()
+                    .create(Api::class.java)
+        }
+    }
 }
