07c20285525cf7c7cdcdeca18db2323e6e3bd9c0
Artsiom <artkostm@gmail.com>
2018-02-20 15:58:20 +0300

    TODO: update this file to use immutable entities

--- app/src/main/kotlin/com/samtakoj/schedule/model/ScheduleModel.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/model/ScheduleModel.kt
@@ -19,7 +19,7 @@ data class TimeCsv(@Id var id: Long = 0, val routeId: Long, val intervalCount: I
                    @Convert(converter = LongListConverter::class, dbType = String::class) val timeTable: List<Long> = listOf(),
                    @Convert(converter = WorkDayConverter::class, dbType = String::class) val workDay: List<WorkDay> = listOf())
 @Entity
-data class StopCsv(@Id(assignable = true) var id: Long = 0, var name: String, var lng: Long, var ltd: Long)
+data class StopCsv(@Id(assignable = true) var id: Long = 0, val name: String, val lng: Long, val ltd: Long)
 @Entity
 data class RouteCsv(@Id(assignable = true) var id: Long = 0, var num: String, var transportType: String,
                     var name: String, var weekDays: String, var stops: String)
