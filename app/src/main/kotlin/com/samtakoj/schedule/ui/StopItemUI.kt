a3a358192a0eb962d96160b261d1d68783400fcb
Artsiom <artkostm@gmail.com>
2018-04-08 22:17:48 +0300

    add stop item ui

---
+++ app/src/main/kotlin/com/samtakoj/schedule/ui/StopItemUI.kt
@@ -0,0 +1,27 @@
+import android.view.*
+import org.jetbrains.anko.*
+import com.samtakoj.schedule.R
+
+class StopItemUI : AnkoComponent<ViewGroup> {
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui) {
+            relativeLayout {
+                backgroundResource = R.color.textColorPrimary
+//                orientation = LinearLayout.VERTICAL
+
+                textView("TextView") {
+                    id = Ids.stopTextView
+                    textColor = R.color.colorPrimary
+                    textSize = 20f
+                }.lparams(width = wrapContent, height = wrapContent) {
+                    centerHorizontally()
+                    centerVertically()
+                }
+            }
+        }
+    }
+
+    private object Ids {
+        val stopTextView = View.generateViewId()
+    }
+}
