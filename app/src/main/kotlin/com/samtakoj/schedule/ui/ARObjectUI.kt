8744a92a4d48386a376d8b14902485eac0ffdfb3
Artsiom <artkostm@gmail.com>
2018-04-08 22:17:36 +0300

    add object ui

---
+++ app/src/main/kotlin/com/samtakoj/schedule/ui/ARObjectUI.kt
@@ -0,0 +1,27 @@
+import android.view.*
+import android.widget.*
+import org.jetbrains.anko.*
+import com.samtakoj.schedule.R
+
+class ARObjectUI : AnkoComponent<ViewGroup> {
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui) {
+            linearLayout {
+                orientation = LinearLayout.VERTICAL
+                weightSum = 1f
+
+                textView("TextView") {
+                    backgroundResource = android.R.color.black
+                    id = Ids.info
+                    textColor = R.color.colorPrimary
+                }.lparams(width = wrapContent, height = wrapContent) {
+                    weight = 1f
+                }
+            }
+        }
+    }
+
+    private object Ids {
+        val info = View.generateViewId()
+    }
+}
