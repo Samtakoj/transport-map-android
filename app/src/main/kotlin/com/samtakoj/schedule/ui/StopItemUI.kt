33e6d1719a4d2525d1b38a60b082b1178db356d3
Artsiom <artkostm@gmail.com>
2018-04-10 22:47:43 +0300

    update stop item ui

--- app/src/main/kotlin/com/samtakoj/schedule/ui/StopItemUI.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/ui/StopItemUI.kt
@@ -1,17 +1,18 @@
 package com.samtakoj.schedule.ui
 
 import android.view.*
+import android.widget.TextView
 import org.jetbrains.anko.*
 import com.samtakoj.schedule.R
+import kotlin.properties.Delegates
 
 class StopItemUI : AnkoComponent<ViewGroup> {
     override fun createView(ui: AnkoContext<ViewGroup>): View {
+        var stopName: TextView by Delegates.notNull()
         return with(ui) {
             relativeLayout {
                 backgroundResource = R.color.textColorPrimary
-//                orientation = LinearLayout.VERTICAL
-
-                textView("TextView") {
+                stopName = textView("TextView") {
                     id = Ids.stopTextView
                     textColor = R.color.colorPrimary
                     textSize = 20f
@@ -20,10 +21,20 @@ class StopItemUI : AnkoComponent<ViewGroup> {
                     centerVertically()
                 }
             }
+            let {
+                it.view.tag = StopItemViewHolder(stopName)
+                it.view
+            }
         }
     }
 
     private object Ids {
         val stopTextView = View.generateViewId()
+    }
+}
+
+class StopItemViewHolder(val stopText: TextView) {
+    fun bind(name: String) {
+        stopText.text = name
     }
 }
