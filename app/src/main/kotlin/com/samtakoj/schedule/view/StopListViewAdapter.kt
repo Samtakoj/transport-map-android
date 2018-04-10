0a3b79f9715b40dddfcbb5d554749ad279fb8603
Artsiom <artkostm@gmail.com>
2018-04-10 22:48:20 +0300

    stop item adapter

--- app/src/main/kotlin/com/samtakoj/schedule/view/StopListViewAdapter.kt
+++ app/src/main/kotlin/com/samtakoj/schedule/view/StopListViewAdapter.kt
@@ -1,37 +1,25 @@
 package com.samtakoj.schedule.view
 
-import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.TextView
-import com.samtakoj.schedule.R
 import com.samtakoj.schedule.model.StopCsv
+import com.samtakoj.schedule.ui.StopItemUI
+import com.samtakoj.schedule.ui.StopItemViewHolder
+import org.jetbrains.anko.AnkoContext
 
-class StopListViewAdapter(val context: Context , val stops: ArrayList<StopCsv>): BaseAdapter() {
-
-    class ViewHolder(val textView: TextView)
-
+class StopListViewAdapter(val stops: ArrayList<StopCsv>): BaseAdapter() {
     private val mLock = Any()
 
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
-        var viewHolder: ViewHolder
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
         var tempConvertView = convertView
 
         if(tempConvertView == null) {
-            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            tempConvertView = inflater.inflate(R.layout.stop_item, parent, false)
-            viewHolder = ViewHolder(tempConvertView.findViewById(R.id.stopTextView) as TextView)
-            tempConvertView.tag = viewHolder
-        } else {
-            viewHolder = tempConvertView.tag as ViewHolder
+            tempConvertView = StopItemUI().createView(AnkoContext.create(parent?.context, parent))
         }
 
         val item = getItem(position)
-        viewHolder.textView.text = item.name
-
+        (tempConvertView.tag as StopItemViewHolder).bind(item.name)
         return tempConvertView
     }
 
